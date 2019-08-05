1e3c06147c911549af282967a3881c6b0df1a5ab
Ramko9999 <ramapitchala@gmail.com>
2019-08-05 17:50:21 -0500

    Got Registration Screen working. Also Able to connect users to firestore.

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,23 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   bool _isLoginButtonClicked = false;
+  bool _isRegisterButtonClicked = false;
 
-
-  void createNewLoginTemplate(){
+  void createNewLoginTemplate() {
     print("In createNewLoginTemplate");
-    
-    setState((){
+
+    setState(() {
       _isLoginButtonClicked = !_isLoginButtonClicked;
-    }
-    );
+      _isRegisterButtonClicked = false;
+    });
+  }
+
+  void createNewRegisterTemplate() {
+    print("In Create new Register Template");
+    setState(() {
+      _isRegisterButtonClicked = !_isRegisterButtonClicked;
+      _isLoginButtonClicked = false;
+    });
   }
 
   @override
@@ -35,22 +43,42 @@ class MyAppState extends State<MyApp> {
             body: new Stack(
       children: [
         new BackgroundImage('assets/backgrounds/blue_green_grad.png'),
-        new Column(children: [
-          Container(child: Image.asset('assets/logos/deca_logo_blue_bg.png', height:150, width: 150, fit: BoxFit.cover,),
-          ),
-          Center(
-            child: Container(
-              width: 200,
-              child: new RaisedButton(child:Text('LOGIN'), textColor: Colors.white, color: Colors.transparent, onPressed:createNewLoginTemplate,
+        new SingleChildScrollView(
+            child: new Column(children: [
+          Container(
+            child: Image.asset(
+              'assets/logos/deca_logo_blue_bg.png',
+              height: 150,
+              width: 150,
+              fit: BoxFit.cover,
             ),
-          )
-        ),
-        //handles pulling up the login template
-        if (_isLoginButtonClicked) new LoginTemplate()
-        ]),
+          ),
+          if (_isLoginButtonClicked)
+            new LoginTemplate(),
+          if (_isRegisterButtonClicked)
+            new RegisterTemplate(),
+          Center(
+              child: Container(
+            width: 200,
+            child: new RaisedButton(
+              child: Text('LOGIN'),
+              textColor: Colors.white,
+              color: Colors.transparent,
+              onPressed: createNewLoginTemplate,
+            ),
+          )),
+          Center(
+              child: Container(
+                  width: 200,
+                  child: new RaisedButton(
+                    child: Text('REGISTER'),
+                    textColor: Colors.white,
+                    color: Colors.transparent,
+                    onPressed: createNewRegisterTemplate,
+                  ))),
+          //handles pulling up the login template
+        ])),
       ],
     )));
   }
 }
-
-
--- lib/screens/authentication/templates.dart
+++ lib/screens/authentication/templates.dart
@@ -1,92 +1,207 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/rendering.dart';
 
 //login template
-class LoginTemplate extends StatefulWidget{
-
+class LoginTemplate extends StatefulWidget {
   LoginTemplate();
-  
+
   @override
-  State<LoginTemplate> createState(){
+  State<LoginTemplate> createState() {
     return _LoginTemplateState();
   }
 }
 
-class _LoginTemplateState extends State<LoginTemplate>{
+class _LoginTemplateState extends State<LoginTemplate> {
   final _loginFormKey = GlobalKey<FormState>();
   String _username;
   String _password;
+  Firestore _fireStore = Firestore.instance; //database connection
   _LoginTemplateState();
 
-  Widget build(BuildContext context){
-    return Form(key: _loginFormKey,
-     child:
-    Column(children: <Widget>[
-      TextFormField(
-        validator:(val){
-          setState(() {
-            _username = val;
-          });
-          bool dotIsNotIn = _username.indexOf(".") == -1;
-          bool atIsNotIn = _username.indexOf("@") == -1;
-          if (dotIsNotIn || atIsNotIn){
-            return "Invalid Email Type";
-          }
-          return null;
-        },
-        textAlign: TextAlign.center,
-        decoration: new InputDecoration(
-          icon: Icon(Icons.person),
-          labelText: "Username",
-        ),
-      ),
-      TextFormField(
-        validator: (val){
-          setState(() {
-           _password = val; 
-          });
-          bool hasLengthLessThan8 = _password.length < 8;
-          if(hasLengthLessThan8){
-            return "Password less than 8";
-          }
-          return null;
-        },
-        textAlign: TextAlign.center,
-        obscureText: true,
-        decoration: new InputDecoration(
-          icon: Icon(Icons.lock),
-          labelText: "Password"
-        ),
-      ),
-      RaisedButton(child: Text("Login"), onPressed: (){
-        //logging into firebase test
-        if(_loginFormKey.currentState.validate()){
-          try{
-            FirebaseAuth.instance.signInWithEmailAndPassword(email:_username, password: _password);
-            print("Logged in Succesfully");
-          }
-          catch(error, stackTrace){
-            print(error);
-          }
-        }
-      }),
-
-    ],),);
+  String fieldMustNotBeEmpty(val) {
+    if (val == "") {
+      return "Field is empty";
+    }
   }
 
-  
-}
+  void tryToLogin() async {
+    try {
+      AuthResult authResult = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: _username, password: _password);
+      var user = authResult.user;
+      String userIdToken = user.uid;
 
-class RegisterTemplate extends StatefulWidget{
-  @override
-  State<RegisterTemplate> createState(){
-    return _RegisterTemplateState();
-}
-}
-class _RegisterTemplateState extends State<RegisterTemplate>{
+      //it is adding but not able to grab the auto id and check if user is already there
+      if(await _fireStore.collection("Users").document(userIdToken) != null){
+            await _fireStore.collection("Users").add({'username':_username, 'password': _password, 'uid': userIdToken});
+            print("Succesfully added");
+      }
+      else{
+        print("Already added");
+      }
 
-  Widget build(BuildContext context){
       
+      
+
+    } catch (error, stackTrace) {
+      print(error);
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Form(
+      key: _loginFormKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            validator: (val) {
+              fieldMustNotBeEmpty(val);
+              setState(() {
+                _username = val;
+              });
+              bool dotIsNotIn = _username.indexOf(".") == -1;
+              bool atIsNotIn = _username.indexOf("@") == -1;
+              if (dotIsNotIn || atIsNotIn) {
+                return "Invalid Email Type";
+              }
+              return null;
+            },
+            textAlign: TextAlign.center,
+            decoration: new InputDecoration(
+              icon: Icon(Icons.person),
+              labelText: "Username",
+            ),
+          ),
+          TextFormField(
+            validator: (val) {
+              fieldMustNotBeEmpty(val);
+              setState(() {
+                _password = val;
+              });
+              bool hasLengthLessThan8 = _password.length < 8;
+              if (hasLengthLessThan8) {
+                return "Password less than 8";
+              }
+              return null;
+            },
+            textAlign: TextAlign.center,
+            obscureText: true,
+            decoration: new InputDecoration(
+                icon: Icon(Icons.lock), labelText: "Password"),
+          ),
+          RaisedButton(
+              child: Text("Login"),
+              onPressed: () {
+                //logging into firebase test
+                if (_loginFormKey.currentState.validate()) {
+                  tryToLogin();
+                }
+              }),
+        ],
+      ),
+    );
+  }
+}
+
+class RegisterTemplate extends StatefulWidget {
+  @override
+  State<RegisterTemplate> createState() {
+    return _RegisterTemplateState();
+  }
+}
+
+class _RegisterTemplateState extends State<RegisterTemplate> {
+  String _firstName;
+  String _lastName;
+  String _username;
+  String _password;
+  final _registrationFormKey = GlobalKey<FormState>();
+
+  void tryToRegister() {
+    if (_registrationFormKey.currentState.validate()) {
+      try {
+        FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: _username, password: _password);
+      } catch (error, stackTrace) {
+        print(error);
+      }
+    }
+  }
+
+  String fieldMustNotBeEmpty(val) {
+    if (val == "") {
+      return "Field is empty";
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Form(
+          key: _registrationFormKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(labelText: "First Name"),
+                validator: (val) {
+                  fieldMustNotBeEmpty(val);
+                  setState(() => _firstName = val);
+                  return null;
+                },
+              ),
+              TextFormField(
+                textAlign: TextAlign.center,
+                decoration: new InputDecoration(labelText: 'Last Name'),
+                validator: (val) {
+                  fieldMustNotBeEmpty(val);
+                  setState(() => _lastName = val);
+                  return null;
+                },
+              ),
+              TextFormField(
+                validator: (val) {
+                  fieldMustNotBeEmpty(val);
+                  setState(() {
+                    _username = val;
+                  });
+                  bool dotIsNotIn = _username.indexOf(".") == -1;
+                  bool atIsNotIn = _username.indexOf("@") == -1;
+                  if (dotIsNotIn || atIsNotIn) {
+                    return "Invalid Email Type";
+                  }
+                  return null;
+                },
+                textAlign: TextAlign.center,
+                decoration: new InputDecoration(
+                  icon: Icon(Icons.person),
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                validator: (val) {
+                  fieldMustNotBeEmpty(val);
+                  setState(() {
+                    _password = val;
+                  });
+                  bool hasLengthLessThan8 = _password.length < 8;
+                  if (hasLengthLessThan8) {
+                    return "Password less than 8";
+                  }
+                  return null;
+                },
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: new InputDecoration(
+                    icon: Icon(Icons.lock), labelText: "Password"),
+              ),
+              RaisedButton(
+                child: Text("REGISTER"),
+                onPressed: tryToRegister,
+              )
+            ],
+          )),
+    );
   }
 }
